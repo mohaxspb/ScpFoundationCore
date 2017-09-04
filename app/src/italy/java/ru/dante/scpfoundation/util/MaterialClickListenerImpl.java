debafac7578d6cb8cc2023ac9e1829f714902f8a
mohax.spb <mohax.spb@gmail.com>
2017-09-04 17:32:15 +0300

    - add IT app implementation

---
+++ app/src/italy/java/ru/dante/scpfoundation/util/MaterialClickListenerImpl.java
@@ -0,0 +1,40 @@
+package ru.dante.scpfoundation.util;
+
+import ru.dante.scpfoundation.ConstantValuesImpl;
+import ru.kuchanov.scp.downloads.ConstantValues;
+import ru.kuchanov.scpcore.ui.activity.MaterialsActivity;
+import ru.kuchanov.scpcore.ui.base.BaseActivity;
+
+/**
+ * Created by mohax on 24.07.2017.
+ * <p>
+ * for ScpFoundationRu
+ */
+public class MaterialClickListenerImpl implements MaterialsActivity.MaterialClickListener {
+
+    private ConstantValues mConstantValues;
+
+    public MaterialClickListenerImpl(ConstantValues constantValues) {
+        mConstantValues = constantValues;
+    }
+
+    @Override
+    public void onMaterialClick(int position, BaseActivity activity) {
+        switch (position) {
+            case 0:
+                activity.startArticleActivity(mConstantValues.getJokes());
+                break;
+            case 1:
+                activity.startArticleActivity(ConstantValuesImpl.Urls.SCP_EX);
+                break;
+            case 2:
+                activity.startArticleActivity(ConstantValuesImpl.Urls.ANOMALS);
+                break;
+            case 3:
+                activity.startArticleActivity(ConstantValuesImpl.Urls.EVENTS);
+                break;
+            default:
+                throw new RuntimeException("unexpected position in materials list");
+        }
+    }
+}
