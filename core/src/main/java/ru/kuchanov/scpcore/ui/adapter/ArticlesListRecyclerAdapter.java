9b758cdd5b917d779d41236fc4110c19c766193e
mohax.spb <mohax.spb@gmail.com>
2017-09-21 20:38:47 +0300

    - wip

--- core/src/main/java/ru/kuchanov/scpcore/ui/adapter/ArticlesListRecyclerAdapter.java
+++ core/src/main/java/ru/kuchanov/scpcore/ui/adapter/ArticlesListRecyclerAdapter.java
@@ -1,18 +1,28 @@
 package ru.kuchanov.scpcore.ui.adapter;
 
+import android.support.annotation.IntDef;
 import android.support.annotation.StringRes;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import javax.inject.Inject;
 
 import ru.kuchanov.scpcore.BaseApplication;
+import ru.kuchanov.scpcore.Constants;
 import ru.kuchanov.scpcore.R;
 import ru.kuchanov.scpcore.db.model.Article;
 import ru.kuchanov.scpcore.db.model.ArticleTag;
@@ -21,7 +31,11 @@ import ru.kuchanov.scpcore.ui.dialog.SettingsBottomSheetDialogFragment;
 import ru.kuchanov.scpcore.ui.holder.HolderMax;
 import ru.kuchanov.scpcore.ui.holder.HolderMedium;
 import ru.kuchanov.scpcore.ui.holder.HolderMin;
+import ru.kuchanov.scpcore.ui.model.ArticlesListModel;
 import timber.log.Timber;
+
+import static ru.kuchanov.scpcore.Constants.Firebase.RemoteConfigKeys.NATIVE_ADS_LISTS_INTERVAL;
+import static ru.kuchanov.scpcore.Constants.Firebase.RemoteConfigKeys.NATIVE_ADS_LISTS_SOURCE;
 
 /**
  * Created by Dante on 17.01.2016.
@@ -66,9 +80,17 @@ public class ArticlesListRecyclerAdapter extends RecyclerView.Adapter<HolderMin>
         }
     }
 
-    private static final int TYPE_ARTICLE = 0;
-    private static final int TYPE_NATIVE_ADS_AD_MOB = 1;
-    private static final int TYPE_NATIVE_ADS_APPODEAL = 2;
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({ArticleListNodeType.ARTICLE, ArticleListNodeType.NATIVE_ADS_AD_MOB, ArticleListNodeType.NATIVE_ADS_APPODEAL})
+    @interface ArticleListNodeType {
+        int ARTICLE = 0;
+        int NATIVE_ADS_AD_MOB = 1;
+        int NATIVE_ADS_APPODEAL = 2;
+    }
+
+//    private static final int TYPE_ARTICLE = 0;
+//    private static final int TYPE_NATIVE_ADS_AD_MOB = 1;
+//    private static final int TYPE_NATIVE_ADS_APPODEAL = 2;
 
     @Inject
     MyPreferenceManager mMyPreferenceManager;
@@ -76,6 +98,8 @@ public class ArticlesListRecyclerAdapter extends RecyclerView.Adapter<HolderMin>
     protected List<Article> mData;
 
     protected List<Article> mSortedWithFilterData = new ArrayList<>();
+
+    protected List<ArticlesListModel> mArticlesAndAds = new ArrayList<>();
 
     private SortType mSortType = SortType.NONE;
 
@@ -197,11 +221,51 @@ public class ArticlesListRecyclerAdapter extends RecyclerView.Adapter<HolderMin>
                 throw new IllegalArgumentException("unexpected type: " + mSortType);
         }
 
+        createDataWithAdsAndArticles();
+
         notifyDataSetChanged();
+    }
+
+    private void createDataWithAdsAndArticles() {
+        // Loop through the items array and place a new Native Express ad in every ith position in
+        // the items List.
+        FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
+        for (int i = 0; i <= mSortedWithFilterData.size(); i += (config.getLong(NATIVE_ADS_LISTS_INTERVAL) - 1)) {
+            View nativeAdView;
+            @Constants.NativeAdsSource
+            int nativeAdsSource = (int) config.getLong(NATIVE_ADS_LISTS_SOURCE);
+
+            switch (nativeAdsSource) {
+                case Constants.NativeAdsSource.ALL:
+                    //show ads from list of sources via random
+                    switch (new Random().nextInt(Constants.NUM_OF_NATIVE_ADS_SOURCES) + 1) {
+                        case Constants.NativeAdsSource.AD_MOB:
+                            nativeAdView = LayoutInflater.from(BaseApplication.getAppInstance()).inflate(R.layout.native_ads_admob, null, false);
+                            mArticlesAndAds.add(i, new );
+                            break;
+                        case Constants.NativeAdsSource.APPODEAL:
+                            nativeAdView = LayoutInflater.from(BaseApplication.getAppInstance()).inflate(R.layout.native_ads_appodeal, null, false);
+                            break;
+                        default:
+                            throw new IllegalArgumentException("unexpected native ads source: " + nativeAdsSource);
+                    }
+
+                    break;
+                case Constants.NativeAdsSource.AD_MOB:
+                    showAdMobNativeAds();
+                    break;
+                case Constants.NativeAdsSource.APPODEAL:
+                    showAppodealNativeAds();
+                    break;
+                default:
+                    throw new IllegalArgumentException("unexpected native ads source: " + nativeAdsSource);
+            }
+        }
     }
 
     @Override
     public long getItemId(int position) {
+        //TODO create correct ID, as we now have ads in list
         return mSortedWithFilterData.get(position).url.hashCode();
     }
 
---
+++ core/src/main/java/ru/kuchanov/scpcore/ui/model/ArticlesListModel.java
@@ -0,0 +1,21 @@
+package ru.kuchanov.scpcore.ui.model;
+
+/**
+ * Created by mohax on 21.09.2017.
+ * <p>
+ * for ScpCore
+ */
+public class ArticlesListModel {
+
+    public int type;
+
+    public Object data;
+
+    @Override
+    public String toString() {
+        return "ArticlesListModel{" +
+                "type=" + type +
+                ", data=" + data +
+                '}';
+    }
+}
