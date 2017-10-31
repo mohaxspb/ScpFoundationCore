77b4251d9a14f9014a6e2b01921bc5b4c5d9f372
mohax.spb <mohax.spb@gmail.com>
2017-10-31 21:18:58 +0300

    - attempt to fix versioning

--- app/src/ru/java/ru/dante/scpfoundation/MyApplicationImpl.java
+++ app/src/ru/java/ru/dante/scpfoundation/MyApplicationImpl.java
@@ -1,5 +1,7 @@
 package ru.dante.scpfoundation;
 
+import android.content.pm.PackageManager;
+
 import ru.dante.scpfoundation.di.DaggerAppComponentImpl;
 import ru.dante.scpfoundation.di.module.HelpersModuleImpl;
 import ru.dante.scpfoundation.di.module.NetModuleImpl;
@@ -7,6 +9,8 @@ import ru.dante.scpfoundation.di.module.StorageModuleImpl;
 import ru.kuchanov.scpcore.BaseApplication;
 import ru.kuchanov.scpcore.di.AppComponent;
 import ru.kuchanov.scpcore.di.module.AppModule;
+import ru.kuchanov.scpcore.util.SystemUtils;
+import timber.log.Timber;
 
 /**
  * Created by mohax on 01.01.2017.
@@ -14,6 +18,23 @@ import ru.kuchanov.scpcore.di.module.AppModule;
  * for scp_ru
  */
 public class MyApplicationImpl extends BaseApplication {
+
+    //TODO create getter for appCode and name
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        try {
+            String packageName = getPackageName();
+            Timber.d("packageName: %s", packageName);
+            long versionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+            Timber.d("versionCode: %s", versionCode);
+            Timber.d("versionCode: %s", BuildConfig.);
+            Timber.d("SystemUtils.getPackageInfo().versionCode: %s", SystemUtils.getPackageInfo().versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected AppComponent buildAppComponentImpl() {
