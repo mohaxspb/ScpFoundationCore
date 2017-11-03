e7cd5e7a42566fe58096c0f3dd9d50e3fae33124
mohax.spb <mohax.spb@gmail.com>
2017-11-03 17:30:45 +0300

    - update app versions  - add ALL app realm migrations

--- app/src/de/java/ru/dante/scpfoundation/di/module/StorageModuleImpl.java
+++ app/src/de/java/ru/dante/scpfoundation/di/module/StorageModuleImpl.java
@@ -40,6 +40,19 @@ public class StorageModuleImpl extends StorageModule {
                 oldVersion++;
             }
 
+            if (oldVersion == 2) {
+                RealmObjectSchema articleSchema = schema.get(Article.class.getSimpleName());
+                if (articleSchema != null) {
+                    articleSchema
+                            .addField(Article.FIELD_IS_IN_OBJECTS_FR, long.class)
+                            .addField(Article.FIELD_IS_IN_OBJECTS_JP, long.class)
+                            .addField(Article.FIELD_IS_IN_OBJECTS_ES, long.class)
+                            .addField(Article.FIELD_IS_IN_OBJECTS_PL, long.class)
+                            .addField(Article.FIELD_IS_IN_OBJECTS_DE, long.class);
+                }
+                oldVersion++;
+            }
+
             //add new if blocks if schema changed
             if (oldVersion < newVersion) {
                 throw new IllegalStateException(String.format(Locale.ENGLISH, "Migration missing from v%d to v%d", oldVersion, newVersion));
