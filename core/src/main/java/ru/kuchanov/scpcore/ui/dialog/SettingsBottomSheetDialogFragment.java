cd5c55de14c12bcb1107dd4e3f54e6e2f196118f
yuri <y.kuchanov@redmadrobot.com>
2018-04-21 19:22:15 +0300

    - realize settings for downloads  - refactoring

--- core/src/main/java/ru/kuchanov/scpcore/ui/dialog/SettingsBottomSheetDialogFragment.java
+++ core/src/main/java/ru/kuchanov/scpcore/ui/dialog/SettingsBottomSheetDialogFragment.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -54,18 +55,18 @@ public class SettingsBottomSheetDialogFragment
         extends BaseBottomSheetDialogFragment
         implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    @StringDef({
-            ListItemType.MIN,
-            ListItemType.MIDDLE,
-            ListItemType.MAX
-    })
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface ListItemType {
-
-        String MIN = "MIN";
-        String MIDDLE = "MIDDLE";
-        String MAX = "MAX";
+    public static BottomSheetDialogFragment newInstance() {
+        return new SettingsBottomSheetDialogFragment();
     }
+
+    @Inject
+    MyPreferenceManager mMyPreferenceManager;
+
+    @Inject
+    MyNotificationManager mMyNotificationManager;
+
+    @Inject
+    InAppHelper mInAppHelper;
 
     //design
     @BindView(R2.id.listItemStyle)
@@ -102,21 +103,20 @@ public class SettingsBottomSheetDialogFragment
     @BindView(R2.id.offlineRandomTextView)
     TextView offlineRandomTextView;
 
+    //downloads
+    @BindView(R2.id.downloadsForceUpdateSwitch)
+    SwitchCompat downloadsForceUpdateSwitch;
+
+    @BindView(R2.id.downloadInnerDepthValueTextView)
+    TextView downloadInnerDepthValueTextView;
+
+    @BindView(R2.id.downloadsDepthSeekbar)
+    SeekBar downloadsDepthSeekbar;
+
+    //downloads END
+
     @BindView(R2.id.buy)
     TextView mActivateAutoSync;
-
-    @Inject
-    MyPreferenceManager mMyPreferenceManager;
-
-    @Inject
-    MyNotificationManager mMyNotificationManager;
-
-    @Inject
-    InAppHelper mInAppHelper;
-
-    public static BottomSheetDialogFragment newInstance() {
-        return new SettingsBottomSheetDialogFragment();
-    }
 
     @Override
     protected int getLayoutResId() {
@@ -240,6 +240,30 @@ public class SettingsBottomSheetDialogFragment
         offlineRandomTextView.setText(randomLabel);
         randomOfflineIsOnSwitch.setOnCheckedChangeListener((compoundButton, checked) -> mMyPreferenceManager.setOfflineRandomEnabled(checked));
 
+        //downloads
+        downloadsForceUpdateSwitch.setChecked(mMyPreferenceManager.isDownloadForceUpdateEnabled());
+        downloadsForceUpdateSwitch.setOnCheckedChangeListener((compoundButton, checked) -> mMyPreferenceManager.setDownloadForceUpdateEnabled(checked));
+
+        downloadsDepthSeekbar.setMax(MyPreferenceManager.MAX_DOWNLOADS_DEPTH);
+        downloadsDepthSeekbar.setProgress(mMyPreferenceManager.getInnerArticlesDepth());
+        downloadsDepthSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(final SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(final SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(final SeekBar seekBar, final int progress, final boolean fromUser) {
+                downloadInnerDepthValueTextView.setText(String.valueOf(progress));
+                mMyPreferenceManager.setInnerArticlesDepth(progress);
+            }
+        });
+        downloadInnerDepthValueTextView.setText(String.valueOf(mMyPreferenceManager.getInnerArticlesDepth()));
+        //downloads END
+
         //hide activate subs for good users
         final boolean noFullSubscription = !mMyPreferenceManager.isHasSubscription();
         mActivateAutoSync.setVisibility(noFullSubscription ? View.VISIBLE : View.GONE);
@@ -309,5 +333,18 @@ public class SettingsBottomSheetDialogFragment
                 //do nothing
                 break;
         }
+    }
+
+    @StringDef({
+            ListItemType.MIN,
+            ListItemType.MIDDLE,
+            ListItemType.MAX
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ListItemType {
+
+        String MIN = "MIN";
+        String MIDDLE = "MIDDLE";
+        String MAX = "MAX";
     }
 }
--- core/src/main/java/ru/kuchanov/scpcore/ui/dialog/TextSizeDialogFragment.java
+++ core/src/main/java/ru/kuchanov/scpcore/ui/dialog/TextSizeDialogFragment.java
@@ -23,8 +23,8 @@ import ru.kuchanov.scpcore.manager.MyPreferenceManager;
 import timber.log.Timber;
 
 
-public class TextSizeDialogFragment
-        extends BaseBottomSheetDialogFragment {
+public class TextSizeDialogFragment extends BaseBottomSheetDialogFragment {
+
     public static final String TAG = TextSizeDialogFragment.class.getSimpleName();
 
     private static final String EXTRA_TEXT_SIZE_TYPE = "EXTRA_TEXT_SIZE_TYPE";
@@ -34,32 +34,35 @@ public class TextSizeDialogFragment
 
     @BindView(R2.id.title)
     TextView title;
+
     @BindView(R2.id.seekbarUi)
     SeekBar seekbarUI;
+
     @BindView(R2.id.seekbarArticle)
     SeekBar seekbarArticle;
+
     @BindView(R2.id.textSizeUi)
     TextView tvUi;
+
     @BindView(R2.id.textSizeArticle)
     TextView tvArticle;
 
     @TextSizeType
     private String mType;
 
-    public static BottomSheetDialogFragment newInstance(@TextSizeType String type) {
-        BottomSheetDialogFragment fragment = new TextSizeDialogFragment();
-        Bundle args = new Bundle();
+    public static BottomSheetDialogFragment newInstance(@TextSizeType final String type) {
+        final BottomSheetDialogFragment fragment = new TextSizeDialogFragment();
+        final Bundle args = new Bundle();
         args.putString(EXTRA_TEXT_SIZE_TYPE, type);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        @TextSizeType
-        String value = getArguments().getString(EXTRA_TEXT_SIZE_TYPE);
+        @TextSizeType final String value = getArguments().getString(EXTRA_TEXT_SIZE_TYPE);
         mType = value;
     }
 
@@ -69,7 +72,7 @@ public class TextSizeDialogFragment
     }
 
     @Override
-    public void setupDialog(Dialog dialog, int style) {
+    public void setupDialog(final Dialog dialog, final int style) {
         super.setupDialog(dialog, style);
 
         switch (mType) {
@@ -90,24 +93,23 @@ public class TextSizeDialogFragment
         }
 
         seekbarUI.setMax(150);
-        float scaleUI = mMyPreferenceManager.getUiTextScale();
+        final float scaleUI = mMyPreferenceManager.getUiTextScale();
 
-        int curProgressUI = (int) ((scaleUI - 0.50f) * 100);
+        final int curProgressUI = (int) ((scaleUI - 0.50f) * 100);
         seekbarUI.setProgress(curProgressUI);
         seekbarUI.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onStopTrackingTouch(final SeekBar seekBar) {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            public void onStartTrackingTouch(final SeekBar seekBar) {
             }
 
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float size = (progress / 100f) + 0.50f;
-                int textSizePrimaryInDp = (int) (getResources().getDimension(R.dimen.text_size_primary)
-                        / getResources().getDisplayMetrics().density);
+            public void onProgressChanged(final SeekBar seekBar, final int progress, final boolean fromUser) {
+                final float size = (progress / 100f) + 0.50f;
+                final int textSizePrimaryInDp = (int) (getResources().getDimension(R.dimen.text_size_primary) / getResources().getDisplayMetrics().density);
 //                Timber.d("text_size_primary: %s", textSizePrimaryInDp);
                 tvUi.setTextSize(size * textSizePrimaryInDp);
                 mMyPreferenceManager.setUiTextScale(size);
@@ -115,23 +117,22 @@ public class TextSizeDialogFragment
         });
 
         seekbarArticle.setMax(150);
-        float scaleArt = mMyPreferenceManager.getArticleTextScale();
-        int curProgressArt = (int) ((scaleArt - 0.50f) * 100);
+        final float scaleArt = mMyPreferenceManager.getArticleTextScale();
+        final int curProgressArt = (int) ((scaleArt - 0.50f) * 100);
         seekbarArticle.setProgress(curProgressArt);
         seekbarArticle.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onStopTrackingTouch(final SeekBar seekBar) {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+            public void onStartTrackingTouch(final SeekBar seekBar) {
             }
 
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float size = (progress / 100f) + 0.50f;
-                int textSizePrimaryInDp = (int) (getResources().getDimension(R.dimen.text_size_primary)
-                        / getResources().getDisplayMetrics().density);
+            public void onProgressChanged(final SeekBar seekBar, final int progress, final boolean fromUser) {
+                final float size = (progress / 100f) + 0.50f;
+                final int textSizePrimaryInDp = (int) (getResources().getDimension(R.dimen.text_size_primary) / getResources().getDisplayMetrics().density);
                 tvArticle.setTextSize(size * textSizePrimaryInDp);
                 mMyPreferenceManager.setArticleTextScale(size);
             }
@@ -146,6 +147,7 @@ public class TextSizeDialogFragment
     @Retention(RetentionPolicy.SOURCE)
     @StringDef({TextSizeType.UI, TextSizeType.ARTICLE, TextSizeType.ALL})
     public @interface TextSizeType {
+
         String UI = "UI";
         String ARTICLE = "ARTICLE";
         String ALL = "ALL";
